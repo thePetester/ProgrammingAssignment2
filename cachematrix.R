1726c2fdb96097b04bf87f7d6d457bca0ac213a8
thePetester <pete_pelter@vanguard.com>
2014-12-11 07:18:48 -0500

    Final Check-in
    Discover and extra curly brace which did not cause any errors.  Also
    fixes curly brace alignment and cleaned up doco a little more.

--- cachematrix.R
+++ cachematrix.R
@@ -6,42 +6,44 @@
 
 ## Creates a list of functions that
 ## can cache the inverse of a matrix.
-makeCacheMatrix <- function(x = matrix()) {
-	# sets the value of m to NULL (provides a default if cacheSolve has not yet been used)
+makeCacheMatrix <- function(x = matrix()) 
+{
+      # sets the value of m to NULL (provides a default if cacheSolve has not yet been used)
       m <- NULL
 
-	#set the value of the matrix
-	set <- function(y) {
-          ## caches the inputted matrix so that cacheSolve can check 
-          ##whether it has changed (note this is within the setmatrix function)
-          x <<- y
+      #set the value of the matrix
+      set <- function(y) 
+      {
+            ## caches the inputted matrix so that cacheSolve can check 
+            ##whether it has changed (note this is within the setmatrix function)
+            x <<- y
 
-          ##sets the value of m (the matrix inverse if used cacheSolve) to NULL
-          m <<- NULL
+            ##sets the value of m (the matrix inverse if used cacheSolve) to NULL
+            m <<- NULL
       }
 
       ##set the value of the matrix
       get <- function() x
       setInverse <- function(inverse) m <<-inverse
-	getInverse <- function() m
+      getInverse <- function() m
 	
       ##creates a list to hold the four functions
       list(set = set, get = get,
            setInverse = setInverse,
            getInverse = getInverse)
-
-  }
 }
 
 ## Return a matrix that is the inverse of 'x'
 ## Computes the inverse of the matrix returned by makeCacheMatrix(). 
 ## If the inverse has already been calculated it retrieves from the cache.
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
       m <- x$getInverse()
    
       ##if the matrix has already been populated return it
       ##without repopulating and recalculating
-      if ( ! is.null(m)) {
+      if ( ! is.null(m)) 
+      {
           print("getting cached data")
           return(m)
       }
@@ -54,13 +56,13 @@ cacheSolve <- function(x, ...) {
 }
 
 ##Test cases, please uncomment to run.
-##a <- makeCacheMatrix(matrix(1:4,2))
-##a$get()
-##a$getInverse()
-##a$set(matrix(5:8,2))
-##a$get()
-##cacheSolve(a)
-##cacheSolve(a)
-##a$getInverse()
-##b = a$getInverse()
-##a$get() %*% b
+a <- makeCacheMatrix(matrix(1:4,2))
+a$get()
+a$getInverse()
+a$set(matrix(5:8,2))
+a$get()
+cacheSolve(a)
+cacheSolve(a)
+a$getInverse()
+b = a$getInverse()
+a$get() %*% b
